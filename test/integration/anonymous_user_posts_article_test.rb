02583690947800bc16c06a7331d81b91adf6db87
shaynarang <shaynarang@gmail.com>
2013-06-04 10:36:09 -0500

    user can browse a list of posts

---
+++ test/integration/anonymous_user_posts_article_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class AnonymousUserPostsArticleTest < ActionDispatch::IntegrationTest
+
+  test "page_includes_title_and_body_content" do
+    Post.create(title: "My First Post", body: "This could be the start of something special.")
+    Post.create(title: "My Second Post", body: "Nope. Still nothing special.")
+    visit '/posts'
+    assert_equal 2, Post.count
+    assert_include page.body, "All Posts"
+    assert_include page.body, "My First Post"
+    assert_include page.body, "My Second Post"
+  end
+
+end
